65ef1ef384e4948bab14649ef3b66825ab24827c
zsb <1109179198@qq.com>
2017-09-06 10:37:22 +0800

    添加vector.h

---
+++ CSAPP/code/link/vector.h
@@ -0,0 +1,12 @@
+//
+// Created by zsb on 17/9/6.
+//
+
+#ifndef OSLEARN_VECTOR_H
+#define OSLEARN_VECTOR_H
+void addvec(int *x, int *y,
+            int *z, int n);
+void multvec(int *x, int *y,
+             int *z, int n);
+
+#endif //OSLEARN_VECTOR_H
